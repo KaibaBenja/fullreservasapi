01e2007f838513b60480669e64145c510885a093
nicolasboattini <38139882+nicolasboattini@users.noreply.github.com>
2025-05-10 18:06:34 -0300

    Update: Add swaggerUiOptiones config

--- src/swagger/swagger.ts
+++ src/swagger/swagger.ts
@@ -1,10 +1,11 @@
 import swaggerJSDoc, { OAS3Definition, OAS3Options } from "swagger-jsdoc";
+import { SwaggerOptions, SwaggerUiOptions } from "swagger-ui-express";
 
 const swaggerDefinition: OAS3Definition = {
     openapi: "3.0.4",
     info: {
         title: "Documentacion Fullreservas API",
-        version: "3.0.0",
+        version: "1.0.0",
         description: "API de Sistema de Reservas",
         contact: {
             name: "Leandro Schugurensky, Benjamín Sánchez Morales, Martin Bosch & Nicolás Boattini",
@@ -22,10 +23,8 @@ const swaggerDefinition: OAS3Definition = {
         }
     ]
 };
-    
-    
 
-const swaggerOptions: OAS3Options = {
+const swaggerSpecs: OAS3Options = {
     swaggerDefinition,
     apis: [
         "./src/modules/address/routes/*.ts",
@@ -36,4 +35,16 @@ const swaggerOptions: OAS3Options = {
     ]
 };
 
-export default swaggerJSDoc(swaggerOptions);
+const swaggerOptions: SwaggerOptions = {
+    docExpansion: "none",
+    displayRequestDuration: true,
+    defaultModelsExpandDepth: 1
+}
+
+export const swaggerUiOptions: SwaggerUiOptions = {
+    customCssUrl: "/swagger-ui/SwaggerDark.css",
+    customSiteTitle: "Fullreservas API Docs",
+    swaggerOptions
+};
+
+export default swaggerJSDoc(swaggerSpecs);
