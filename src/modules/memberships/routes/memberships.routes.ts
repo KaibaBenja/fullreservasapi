2b6df0d85681842ade6bc8d07611e4f12ee18057
nicolasboattini <38139882+nicolasboattini@users.noreply.github.com>
2025-05-11 21:13:09 -0300

    Swagger membership docs: Scheme & Tag

--- src/modules/memberships/routes/memberships.routes.ts
+++ src/modules/memberships/routes/memberships.routes.ts
@@ -1,10 +1,62 @@
 import express from "express";
-import membershipsController from "../controllers/memberships.controller";
 import { validateSchema, validateSchemaPartial } from "../../../middlewares/validateSchema";
+import membershipsController from "../controllers/memberships.controller";
 import { membershipsSchema } from "../schemas/memberships.schema";
 
 export const membershipRoutes = express.Router();
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      Memberships:
+ *          type: object
+ *          required:
+ *              - id
+ *              - user_id
+ *              - tier
+ *              - status
+ *          properties:
+ *              id:
+ *                  type: binary
+ *                  format: uuid
+ *                  example: 550e8400-e29b-41d4-a716-446655440000
+ *              user_id:
+ *                  type: binary
+ *                  format: uuid
+ *                  example: 550e8400-e29b-41d4-a716-446655440000
+ *              tier:
+ *                  type: string
+ *                  enum: [FREE, BASIC, ADVANCED, PREMIUM]
+ *                  example: FREE
+ *              status:
+ *                  type: string
+ *                  enum: [ACTIVE, INACTIVE, PENDING, EXPIRED, DELAYED]
+ *                  example: EXPIRED
+ *              expire_date:
+ *                  type: datetime
+ *                  example: 25/05/2025 11:00:00.000
+ *              created_at:
+ *                  type: datetime
+ *                  example: 25/05/2025 11:00:00.000
+ *              updated_at:
+ *                  type: datetime
+ *                  example: 25/05/2025 11:00:00.000
+ *          example:
+ *              id: 550e8400-e29b-41d4-a716-446655440000
+ *              user_id: 550e8400-e29b-41d4-a716-446655440000
+ *              tier: FREE
+ *              status: EXPIRED
+ *              expire_date: 25/05/2025 11:00:00.000
+ *              created_at: 25/05/2025 11:00:00.000
+ *              updated_at: 25/05/2025 11:00:00.000
+ */
 
+/**
+ * @swagger
+ * tags:
+ *  name: Membership
+ *  description: Membresías de Comerciantes
+ */
 membershipRoutes.post("/", validateSchema(membershipsSchema), membershipsController.create);
 membershipRoutes.get("/", membershipsController.getAll);
 membershipRoutes.post("/filtered", validateSchemaPartial(membershipsSchema), membershipsController.getAllByFilters);
