45f29084f581bf4a88b73c180a778ad856fa3508
nicolasboattini <38139882+nicolasboattini@users.noreply.github.com>
2025-05-25 02:54:06 -0300

    Add: New Models Associations

--- src/config/sequelize/initAssociations.ts
+++ src/config/sequelize/initAssociations.ts
@@ -2,6 +2,8 @@ import Address from "../../modules/address/models/addresses.model";
 import Country from "../../modules/address/models/countries.model";
 import Province from "../../modules/address/models/provinces.model";
 import Booking from "../../modules/bookings/models/bookings.model";
+import Membership from "../../modules/memberships/models/memberships.model";
+import MembershipPlan from "../../modules/memberships/models/membershipsPlans.model";
 import AvailableSlot from "../../modules/shops/models/availableSlots.model";
 import ClosedDay from "../../modules/shops/models/closedDays.model";
 import Image from "../../modules/shops/models/images.model";
@@ -11,9 +13,11 @@ import Schedule from "../../modules/shops/models/schedules.model";
 import ShopAddress from "../../modules/shops/models/shopAddresses.model";
 import Shop from "../../modules/shops/models/shops.model";
 import Subcategory from "../../modules/shops/models/subcategories.model";
+import MerchantSetting from "../../modules/users/models/merchants.model";
+import OperatorSetting from "../../modules/users/models/operators.model";
+import Role from "../../modules/users/models/roles.model";
+import UserRole from "../../modules/users/models/userroles.model";
 import User from "../../modules/users/models/users.model";
-import Membership from "../../modules/memberships/models/memberships.model";
-import MembershipPlan from "../../modules/memberships/models/membershipsPlans.model";
 
 export function initAssociations() {
     Province.belongsTo(Country, { foreignKey: "country_id", onDelete: "CASCADE" });
@@ -57,6 +61,24 @@ export function initAssociations() {
     User.hasMany(Rating, { foreignKey: 'user_id', as: 'rating' });
     Rating.belongsTo(User, { foreignKey: 'user_id' });
 
+    User.hasOne(Membership, {foreignKey: 'user_id', as: 'membership' });
+    Membership.belongsTo(User, {foreignKey: 'user_id'});
+
     MembershipPlan.hasMany(Membership, { foreignKey: 'tier', sourceKey: 'id' });
-    Membership.belongsTo(MembershipPlan, {foreignKey: 'tier', targetKey: 'id'});
+    Membership.belongsTo(MembershipPlan, {foreignKey: 'tier', targetKey: 'id', as: 'membership_plan'});
+    
+    User.belongsToMany(Role, { through: UserRole, foreignKey: 'user_id', otherKey: 'role_id', as: 'roles'});
+    Role.belongsToMany(User, { through: UserRole, foreignKey: 'role_id', otherKey: 'user_id'});
+
+    User.hasOne(OperatorSetting, {  foreignKey: 'user_id',  as: 'operator'});
+    OperatorSetting.belongsTo(User, {  foreignKey: 'user_id',  as: 'user'});
+
+    Shop.hasMany(OperatorSetting, {  foreignKey: 'shop_id',  as: 'operator'});
+    OperatorSetting.belongsTo(Shop, {  foreignKey: 'shop_id',  as: 'shop'});
+
+    User.hasMany(Shop, {foreignKey: 'user_id', as: 'shop'});
+    Shop.belongsTo(User, {foreignKey: 'user_id', as: 'user'});
+
+    User.hasOne(MerchantSetting, {foreignKey: 'user_id', as: 'merchant'});
+    MerchantSetting.belongsTo(User, {foreignKey: 'user_id', as: 'user'});
 }
