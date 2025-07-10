89b2015a9abd4a4249d2bc5c830c7cbe2dfb53f7
Nicolás Boattini <38139882+nicolasboattini@users.noreply.github.com>
2025-07-10 19:03:01 -0300

    Update auth.controller.ts

--- src/modules/users/controllers/auth.controller.ts
+++ src/modules/users/controllers/auth.controller.ts
@@ -181,7 +181,15 @@ export const loginWithGoogle: RequestHandler = async (req, res) => {
 export const login: RequestHandler = async (req, res) => {
   try {
     const user = await usersServices.auth.loginUser(req.body);
-    res.status(200).json(user);
+    if (!user) {
+      return handleErrorResponse(res, 404, "Usuario no registrado.");
+    }
+    if (user.user.roles && user.user.roles.length > 0 && user.user.roles[0] === "OPERATOR") {
+      const operator = await usersServices.operators.getByUserId({ user_id: user.user.id.toString("utf-8") });
+      res.status(200).json({user, operator});
+    } else {
+      res.status(200).json(user);
+    }
   } catch (error) {
     res.status(500).json({
       message: error instanceof Error ? error.message : "Error logging in",
